16b88ab01d007ac900bd8e0692b57bea1593c428
jyostnareddy2006-hue <jyostnareddy2006@gmail.com>
2026-03-03 18:16:35 +0530

    Add function to calculate grades based on marks

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def calculate_grade(marks):
+    if marks >= 90:
+        return "A+"
+    elif marks >= 75:
+        return "A"
+    elif marks >= 60:
+        return "B"
+    elif marks >= 50:
+        return "C"
+    else:
+        return "Fail"
